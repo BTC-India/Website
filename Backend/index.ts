60315ccfa3352c4ef2ede6ead6b7da2bfea837bf
Manas <iamscientistmanas@gmail.com>
2024-09-29 12:21:19 +0530

    Add initial files

---
+++ Backend/index.ts
@@ -0,0 +1,40 @@
+import express, { Express, Request, Response } from "express";
+import cors from "cors";
+import { config } from "dotenv";
+import { connectToDB } from "./config/db";
+// Import routes
+import emailerRoute from "./routes/emailer";
+
+// Load the env
+config();
+
+// connect to Database
+connectToDB();
+
+// configure app
+const app: Express = express();
+const PORT: string = process.env.PORT || "5000";
+
+// CORS configuration
+const corsConfiguration = {
+  //   origin: [""],
+  origin: "*",
+  optionSucessStatus: 200,
+};
+
+// middleware to use import routes and enable cors
+app.use(express.json());
+app.use(cors(corsConfiguration));
+
+// Routes
+app.use("/api/send-email", emailerRoute);
+
+// Landing endpoint
+app.get("/", (req: Request, res: Response) => {
+  res.status(200).json({ message: "200 Ok BTC India Loading :)" });
+});
+
+// Listening at
+app.listen(PORT, () => {
+  console.log(`Server active at port: ${PORT}`);
+});
---
+++ Backend/routes/emailer.ts
@@ -0,0 +1,45 @@
+import { Router, Request, Response } from "express";
+import nodemailer from "nodemailer";
+
+const router = Router();
+const PORT: number = 465;
+const HOST: string = "smtppro.zoho.in";
+
+// Route - 1: Sending mail
+router.post("/send-email", async (req: Request, res: Response) => {
+  // Get the body
+  const { fromName, toName, toEmail, subject, message } = req.body;
+
+  // Get the auth parameters
+  const EMAIL = process.env.EMAIL;
+  const PASSWORD = process.env.PASSWORD;
+
+  // Create transporter
+  const transporter = nodemailer.createTransport({
+    host: HOST,
+    port: PORT,
+    secure: true, //Use SSL
+    auth: {
+      user: EMAIL,
+      pass: PASSWORD,
+    },
+  });
+
+  // Mail options
+  const mailOptions: nodemailer.SendMailOptions = {
+    from: `${fromName} <${EMAIL}>`,
+    to: `${toName} <${toEmail}>`,
+    subject: subject,
+    text: message,
+  };
+
+  try {
+    await transporter.sendMail(mailOptions);
+    res.status(200).json({ message: "Mail sent successfully" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+});
+
+export default router;
